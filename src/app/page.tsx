72f9c689228c2d1285cdabd8e77f7d49767e932c
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-10-23 19:56:26 -0300

    "Add husky and lint-staged"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+//import { type } from 'os'
+
+export default function Home({ title = 'React-avançado' }) {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          {title}&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
